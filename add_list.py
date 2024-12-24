e534019809ebc53536a144428739dc85a823a2b6
Hardx007 <malinkin.96@mail.ru>
2024-12-24 16:02:27 +0300

    Grade 1. Этап 1: Задание 4

---
+++ add_list.py
@@ -0,0 +1,5 @@
+title_1 = input("Введите 1 заголовок  : " )
+title_2 = input("Введите 2 заголовок : " )
+title_3 = input("Введите 3 заголовок : " )
+title = [title_1, title_2, title_3]
+print(f"Список ваших заголовков : {title}")
