ba25bcaaacf9fe75819144ca0b89d6d37540eee4
Hardx007 <malinkin.96@mail.ru>
2024-12-24 14:16:49 +0300

    Grade 1. Этап 1: Задание 1
    Grade 1. Этап 1: Задание 1

---
+++ greetings.py
@@ -0,0 +1,9 @@
+username = "имя пользователя"
+title = "заголовок заметки"
+content = "описание заметки"
+status = "Cтатус заметки"
+created_date = "Введите дату начала задачи в формате 20.12.2024"
+issue_date = "Введите завершения задачи в формате 22-12-2024"
+print(username, title, content, status, created_date, issue_date)
+
+
